b5aaa783d80f056a6784fa47e19e67ec5ebc1387
Jorge Navarro <jnavarro@okode.com>
2019-09-17 14:50:57 +0200

    Initial commit

---
+++ generate-dt.sh
@@ -0,0 +1,20 @@
+RED='\033[0;31m'
+printf "${RED} Make sure you have replace utils/api.json from Swagger /v2/api-docs"
+
+spectacle --disable-js --target-dir tmp utils/api.json
+wkhtmltopdf tmp/index.html api.pdf > /dev/null 2>&1
+
+echo 'Input range of pages you want to separate form api.pdf'
+read -p 'First Page ' firstPage
+read -p 'Last Page ' lastPage
+mkdir tmpPdf
+pdfseparate -f $1 -l $2 api.pdf tmpPdf/page%d.pdf
+
+pdfunite tmpPdf/* tmpPdf/DT_API.pdf > /dev/null 2>&1
+
+read -p 'Name of project: ' projectName
+pdfunite utils/DT_COVER.pdf tmpPdf/DT_API.pdf DT.DT.APP.IMEDIADOR-${projectName}.pdf
+
+rm -rf api.pdf
+rm -rf tmp
+rm -rf tmpPdf
