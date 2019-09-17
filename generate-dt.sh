567d17e761ad57c0853e9b280731d475046cc5db
Jorge Navarro <jnavarro@okode.com>
2019-09-17 15:19:15 +0200

    Update

--- generate-dt.sh
+++ generate-dt.sh
@@ -1,14 +1,15 @@
 RED='\033[0;31m'
+NC='\033[0m' # No Color
 printf "${RED} Make sure you have replace utils/api.json from Swagger /v2/api-docs\n"
 
 spectacle --disable-js --target-dir tmp utils/api.json > /dev/null 2>&1
 wkhtmltopdf tmp/index.html api.pdf > /dev/null 2>&1
 
-echo 'Input range of pages you want to separate form api.pdf'
+printf "${NC} Input range of pages you want to separate from api.pdf"
 read -p 'First Page ' firstPage
 read -p 'Last Page ' lastPage
 mkdir tmpPdf
-pdfseparate -f $1 -l $2 api.pdf tmpPdf/page%d.pdf
+pdfseparate -f $firstPage -l $lastPage api.pdf tmpPdf/page%d.pdf
 
 pdfunite tmpPdf/* tmpPdf/DT_API.pdf > /dev/null 2>&1
 
